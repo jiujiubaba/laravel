b3c6996b60093770dcadea9b6e10b61aa3f7229c
wuxu <wu.xu@topka.cn>
2015-10-26 18:44:46 +0800

    laravel

---
+++ frontend/asset/js/plugins.js
@@ -0,0 +1,7 @@
+function index(){
+	
+}
+function two()
+{
+	
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
-var gulp = require("gulp");
-var srcPath = './resources/asset/';
-var destPath = './public/asset/';
+var fs = require('fs');
+var path = require('path');
+var merge = require('merge-stream');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
-gulp.task('serve', function(){
-	console.log('ss');
+var srcPath = './resources/assets/';
+var destPath = './frontend/asset/';
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function(file) {
+        return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
+
+gulp.task('scripts', function() {
+   var folders = getFolders(srcPath + 'js');
+
+   var tasks = folders.map(function(folder) {
+      // 拼接成 foldername.js
+      // 写入输出
+      // 压缩
+      // 重命名为 folder.min.js
+      // 再一次写入输出
+      return gulp.src(path.join(srcPath + 'js', folder, '/*.js'))
+        .pipe(concat(folder + '.js'))
+        .pipe(gulp.dest(destPath + 'js/'))
+        .pipe(uglify())
+        .pipe(rename(folder + '.min.js'))
+        .pipe(gulp.dest(destPath + 'js/'));
+   });
+
+   return merge(tasks);
 });
